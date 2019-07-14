4d5fc686ef61b631a739177cf57aea96d5c57dce
yanchuanhao <1198632539@qq.com>
2019-07-14 14:33:33 +0800

    123

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+
+
+
     private UserService userService;
     public String getUser(int id){
         User user = userService.findUser(id);
